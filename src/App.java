8b341764255460566b913324c029a6851ad76b7c
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-07-19 20:08:04 +0430

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int boardWidth = 1400;
+        int boardHeight = 730;
+
+        JFrame fream = new JFrame("ملا نصرالدین");
+
+        fream.setSize(boardWidth, boardHeight);
+        fream.setLocationRelativeTo(null);
+        fream.setResizable(false);
+        fream.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // MollaNasruddin mollaNasruddin = new MollaNasruddin();
+        // fream.add(mollaNasruddin);
+
+        // makes full screen up to header
+        fream.pack();
+        // mollaNasruddin.requestFocus();
+        fream.setVisible(true);
+
     }
 }
